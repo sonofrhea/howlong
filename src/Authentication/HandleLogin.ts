e0dbdafd4241c1cc2b62e091524ce46a18656c92
agbabiaka <sonofrhea@yandex.com>
2025-12-15 20:06:11 +0800

    login1

--- src/Authentication/HandleLogin.ts
+++ src/Authentication/HandleLogin.ts
@@ -1,21 +1,21 @@
-//import apiClient from '../BaseEngine';
-//import { ImportMeta } from '../vite-env';
-//
-//
-//const HandleLogin = async (email: string, password: any) => {
-//    const isDevelopment = import.meta.env.MODE === "development";
-//  const baseEntry = isDevelopment ? import.meta.env.VITE_API_BASE_URL_LOCAL : import.meta.env.VITE_API_BASE_URL_DEPLOY;
-//
-// try {
-//   const response = await apiClient.post(`${baseEntry}core/login/`, {
-//     email,
-//     password
-//   });
-//   localStorage.setItem('Token', response.data.token);
-//   return response.data; // Return user data if needed
-// } catch (error: any) {
-//   console.error('Login failed:', error.response?.data || error.message);
-//   throw error; 
-// }
-//};
-//export default HandleLogin;
+import apiClient from '../BaseEngine';
+import { ImportMeta } from '../vite-env';
+
+
+const HandleLogin = async (email: string, password: any) => {
+    const isDevelopment = import.meta.env.MODE === "development";
+  const baseEntry = isDevelopment ? import.meta.env.VITE_API_BASE_URL_LOCAL : import.meta.env.VITE_API_BASE_URL_DEPLOY;
+
+ try {
+   const response = await apiClient.post(`${baseEntry}core/login/`, {
+     email,
+     password
+   });
+   localStorage.setItem('Token', response.data.token);
+   return response.data; // Return user data if needed
+ } catch (error: any) {
+   console.error('Login failed:', error.response?.data || error.message);
+   throw error; 
+ }
+};
+export default HandleLogin;
