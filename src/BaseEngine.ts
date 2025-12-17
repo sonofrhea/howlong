9a3fd99d1482acaceeca1c6c48e22d7204234e37
agbabiaka <sonofrhea@yandex.com>
2025-12-18 01:10:27 +0800

    dec 18.

--- src/BaseEngine.ts
+++ src/BaseEngine.ts
@@ -9,7 +9,7 @@ const baseEntry = isDevelopment ? import.meta.env.VITE_API_BASE_URL_LOCAL : impo
 // my base configuration
 const apiClient = axios.create({
   baseURL: baseEntry,
-  timeout: 50,
+  timeout: 7000,
   withCredentials: false
 });
 
